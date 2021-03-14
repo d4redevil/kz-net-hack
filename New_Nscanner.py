e6dd1918f61955b8787497c83ec0623c7850160f
kingness-zero <80645023+kingness-zero@users.noreply.github.com>
2021-03-14 19:28:00 +0000

    Update New_Nscanner.py

--- New_Nscanner.py
+++ New_Nscanner.py
@@ -1,20 +1,31 @@
 import subprocess as sp
 import re
 
+
 def nmap_scan(ip):
     final=[]
+    
+    #Scan using nmap
     output=sp.check_output(["sudo","nmap","-sn",ip],universal_newlines=True)
-    print(output)
+            
     #IP Re formula for +.+.+.+
-    #MAC Re Formula \w\w:\w\w:\w\w:\w\w:\w\w:\w\w
-    #Device found by mac \(\S.*\)\n
     ip_search=re.findall(r"for +.+.+.+",output)
+    
+    #To remove unwanted string from results
     for i in range (len(ip_search)):
         ip_search[i]=ip_search[i].replace("for ","")
+    
+    #MAC Re Formula \w\w:\w\w:\w\w:\w\w:\w\w:\w\w
     mac_search=re.findall(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",output)
+
+    #Device found by mac \(\S.*\)\n
     device_search=re.findall(r"\(\S.*\)\n",output)
+    
+    #To remove unwanted string from results
     for i in range (len(device_search)):
         device_search[i]=device_search[i][1:len(device_search[i])-2]
+    
+    #Ziping can remove unwanted string from search results
     for ip,mac,device in zip(ip_search,mac_search,device_search):
         result={}
         result["ip"]=ip
@@ -24,9 +35,16 @@ def nmap_scan(ip):
         
         
     return final
+
+
+
+
 if __name__=="__main__":
-    scan_result=nmap_scan("192.168.1.1/24")#str(input("> enter IP / IP Range : ")))
+    
+    scan_result=nmap_scan(str(input("> enter IP / IP Range : ")))
     print(len(scan_result))
+    
+    #To list the information gathered
     for i in range(len(scan_result)):
         print("hello")
         if scan_result[i]["ip"] == "192.168.1.1":
